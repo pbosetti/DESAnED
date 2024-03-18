d30c54f88fea1a52daecbd6f0986fe34ccf61d62
Paolo Bosetti <paolo.bosetti@unitn.it>
2024-03-18 11:16:43 +0100

    Update gist base address

--- utils.R
+++ utils.R
@@ -15,7 +15,7 @@ if (!exists("read_file")) {
 #' url <- sample_files("duplicate.csv", show=T)
 #' df <- read.csv(url)
 sample_files <- function(file_name, show=FALSE) {
-  url = paste0("https://gist.githubusercontent.com/pbosetti/4e431d369af33b8e8aabd0464d515165/raw/6f696f55fd53054409d941bac633e9f5214c8251/", file_name)
+  url = paste0("https://gist.githubusercontent.com/pbosetti/aff679908e2b99bab724030919a41896/raw/c9406ad3d45674a36bb25ab7ac8fdd85ddb43276/", file_name)
   if (show) {
     txt = read_file(url)
     cat(txt)
